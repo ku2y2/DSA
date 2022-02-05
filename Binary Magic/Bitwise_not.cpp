a8ab619598bdb63b56b0a9c9488661fd06afa2ed
Kundan <kumarkundan2306@gmail.com>
2022-02-06 01:19:34 +0530

    Commit

---
+++ Binary Magic/Bitwise_not.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    //for unsigned number
+    unsigned int a = 5;
+    unsigned int d = 2;
+    int b = 5;
+    int c =2;
+    int e = -8;
+    cout<< "Bitwise not of unsigned int a = "<<(~a)<<endl;
+    cout<< "Bitwise not of unsigned int d = "<<(~d)<<endl;
+    cout<< "Bitwise not of signed int b = "<<(~b)<<endl;
+    cout<< "Bitwise not of signed int c = "<<(~c)<<endl;
+    cout<< "Bitwise not of signed int e = "<<(~e)<<endl;
+}
---
+++ Binary Magic/Right_shift.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a = 33;
+    int b = 8;
+    cout<<"Right shift of a by 1 = "<<(a>>1)<<endl;
+    cout<<"Right shift of a by 2 = "<<(a>>2)<<endl;
+    cout<<"Right shift of a by b = "<<(a>>b)<<endl;
+}
