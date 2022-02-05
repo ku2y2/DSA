988c48f89d55bdf518bbbbeab27981b83ac0e1eb
Kundan <kumarkundan2306@gmail.com>
2022-02-05 23:51:38 +0530

    Commit

---
+++ Binary Magic/Intro_to_AND_OR_XOR.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdtr1c++.h>
+using namespace std;
+int main()
+{
+    int a=3, b=6;
+    cout<<"AND = "<<(a&b)<<endl;
+    cout<<"OR = "<<(a|b)<<endl;
+    cout<<"XOR = "<<(a^b)<<endl;
+}
---
+++ Binary Magic/left_shift.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x= 3;
+    int y = 4;
+    cout<<"Left shift of x by 1 = "<<(x<<1)<<endl;
+    cout<<"Left shift of x by 2 = "<<(x<<2)<<endl;
+    cout<<"Left shift of x by y = "<<(x<<y)<<endl;
+}
